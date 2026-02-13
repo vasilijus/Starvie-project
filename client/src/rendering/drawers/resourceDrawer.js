0f3632a2e12b425b1794220a053cf084eec79b68
Sergei <vasiliokas@gmail.com>
2026-02-13 21:48:10 +0000

    Added various shapes to some items - fix v_2.2.1

--- client/src/rendering/drawers/resourceDrawer.js
+++ client/src/rendering/drawers/resourceDrawer.js
@@ -18,6 +18,19 @@ export function drawResource(ctx, resource, x, y) {
         return { color, renderRadius };
     }
 
+    if (shape.kind === 'mushroom') {
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(x, y, renderRadius, 0, Math.PI * 2);
+        ctx.fill();
+
+        ctx.fillStyle = '#00AA00';
+        const trunkW = Math.max(4, renderRadius * 0.4);
+        const trunkH = Math.max(6, renderRadius * 0.7);
+        ctx.fillRect(x - trunkW / 1, y + renderRadius * 0.25, trunkW, trunkH);
+        return { color, renderRadius };
+    }
+
     if (shape.kind === 'stone') {
         ctx.fillStyle = color;
         ctx.beginPath();
@@ -27,6 +40,42 @@ export function drawResource(ctx, resource, x, y) {
         ctx.strokeStyle = '#5e5e5e';
         ctx.lineWidth = 1;
         ctx.stroke();
+        return { color, renderRadius };
+    }
+
+    if (shape.kind === 'rock') {
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.fillRect(x, y, renderRadius, renderRadius);
+        ctx.strokeStyle = '#5e5e5e';
+        ctx.lineWidth = 1;
+        ctx.stroke();
+        return { color, renderRadius };
+    }
+
+
+
+    if (shape.kind === 'cactus') {
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.ellipse(x, y, renderRadius / 2, renderRadius, 0, 0, Math.PI * 2, true);
+        ctx.fill();
+        ctx.strokeStyle = '#0a5800';
+        ctx.lineWidth = 1;
+        ctx.stroke();
+        return { color, renderRadius };
+    }
+
+    if (shape.kind === 'gem') {
+        const gradient = ctx.createLinearGradient(x-renderRadius/2, y-renderRadius/2,x+renderRadius/2, y+renderRadius/2);
+            gradient.addColorStop(0, "green");
+            gradient.addColorStop(0.5, "cyan");
+            gradient.addColorStop(1, "green");
+        // ctx.fillStyle = color;
+        ctx.fillStyle = gradient;
+        ctx.beginPath();
+        ctx.arc(x, y, renderRadius, 0, Math.PI * 2);
+        ctx.fill();
         return { color, renderRadius };
     }
 
