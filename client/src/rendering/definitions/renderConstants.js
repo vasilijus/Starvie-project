a76c52c4e5e967bfcdd7fe88e30f00689c383409
Sergej Vasiljev <vasiliokas@gmail.com>
2026-02-15 23:39:58 +0000

    Refactor rendering to atlas sprites with 3x scaled visuals

---
+++ client/src/rendering/definitions/renderConstants.js
@@ -0,0 +1,5 @@
+export const RENDER_SCALE = 3;
+
+export function scaleValue(value) {
+    return value * RENDER_SCALE;
+}
---
+++ client/src/rendering/spriteAtlases.js
@@ -0,0 +1,23 @@
+import resourceSheetUrl from '../assets/resource_sprite_sheet.png';
+import enemySheetUrl from '../assets/enemies_sprite_sheet.png';
+
+function createAtlas(src) {
+    const image = new Image();
+    image.src = src;
+    return image;
+}
+
+const resourceAtlas = createAtlas(resourceSheetUrl);
+const enemyAtlas = createAtlas(enemySheetUrl);
+
+export function getResourceAtlasImage() {
+    return resourceAtlas;
+}
+
+export function getEnemyAtlasImage() {
+    return enemyAtlas;
+}
+
+export function isAtlasReady(image) {
+    return Boolean(image && image.complete && image.naturalWidth > 0);
+}
